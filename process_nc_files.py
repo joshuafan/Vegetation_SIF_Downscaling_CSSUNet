743eb73c69c7f8cc21fe07526913c47d16d83323
Joshua Fan <joshuayfan@hotmail.com>
2020-02-05 09:57:30 -0500

    Changes

--- process_nc_files.py
+++ process_nc_files.py
@@ -1,9 +1,52 @@
 from scipy.io import netcdf
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+import numpy as np
+import os
 import xarray as xr
 
 FILE_PATH = './datasets/TROPOMI_SIF_2018/TROPO_SIF_08-2018.nc'
+DATE = '2018-08-01'
+PLOT_FOLDER = './datasets/TROPOMI_SIF_2018/visualizations/'
 
-# single file
+MIN_SIF = 0.0
+MAX_SIF = 1.0
+
 dataset = xr.open_dataset(FILE_PATH)
-dcSIF = dataset.relSIF.sel(time='2018-08-02').values
-print(dcSIF)
+
+# Plot the distribution of dcSIF (across all time/space)
+all_dcSIF = dataset.dcSIF.values.flatten()
+all_dcSIF = all_dcSIF[~np.isnan(all_dcSIF)]
+n, bins, patches = plt.hist(all_dcSIF, 100, facecolor='blue', alpha=0.5)
+plt.title('dcSIF values, August 2018 (over all days / locations, excluding NaN values)')
+plt.xlabel('dcSIF')
+plt.ylabel('Number of pixels (across all days)')
+
+plt.savefig(os.path.join(PLOT_FOLDER, 'all_dcSIF_08-2018.png'))
+plt.close()
+
+# Select one date (but backfill missing data that's present in the next few days, up to 10 days later)
+data_array = dataset.dcSIF.sel(time=DATE)
+# bfill('time', 10)
+# SIF should never be negative, so replace any negatives or NaN with 0
+data_array = data_array.fillna(0)
+data_array = data_array.where(data_array >= 0, 0)
+print(data_array)
+
+
+
+
+plt.figure(figsize=(21,9))
+color_map = plt.get_cmap('Greens')
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.set_global()
+data_array.plot.pcolormesh(ax=ax, transform=ccrs.PlateCarree(), x='lon', y='lat', vmin=MIN_SIF, vmax=MAX_SIF,
+                           cmap=color_map)  # , add_colorbar=False)
+ax.coastlines()
+plt.title('dcSIF for ' + DATE)
+plt.savefig(os.path.join(PLOT_FOLDER, 'TROPOMI_SIF_' + DATE + '_global.png'))
+plt.close()
+
+#ax.set_ylim([0,90]);
+#plt.imshow(dcSIF, interpolation='none')
+#plt.show()
--- process_tif_files.py
+++ process_tif_files.py
@@ -1,7 +1,41 @@
-import tifffile
+# Import all packages in their own cell at the top of your notebook
+import numpy as np
+import os
+import rasterio as rio
+import matplotlib.pyplot as plt
+from rasterio.plot import show
 
-FILE_PATH = "datasets/GEE_data/imageToDriveExample.tif"
+# import earthpy as et
 
-image_stack = tifffile.imread(FILE_PATH)
-print(image_stack.shape)
-print(image_stack.dtype)
+FILE_PATH = "datasets/GEE_data/41-42N_92-93W_reflectance.tif"
+# FILE_PATH = "datasets/GEE_data/44-45N_87-88W_reflectance.tif" # "datasets/GEE_data/41-42N_92-93W_reflectance.tif"  # "datasets/GEE_data/44-45N_87-88W_cdl.tif"
+
+with rio.open(FILE_PATH) as tiff_dataset:
+    print('Bounds:', tiff_dataset.bounds)
+    print('Metadata:', tiff_dataset.meta)
+    print('Resolution:', tiff_dataset.res)
+    print('Number of layers:', tiff_dataset.count)
+    print('Coordinate reference system:', tiff_dataset.crs)
+    print('Shape:', tiff_dataset.shape)
+
+    red = tiff_dataset.read(4) / 1000.
+    green = tiff_dataset.read(3) / 1000.
+    blue = tiff_dataset.read(2) / 1000.
+    rgb = np.stack([red, green, blue], axis=0)
+    print('rgb shape', rgb.shape)
+    show(rgb)
+
+    print(green)
+    plt.imshow(green, cmap='Greens', vmin=0, vmax=1)
+    plt.show()
+
+    left, top = (-88.0, 45.0)
+    right, bottom = (-87.8, 44.8)
+    left_idx, top_idx = tiff_dataset.index(left, top)
+    right_idx, bottom_idx = tiff_dataset.index(right, bottom)
+    print(left_idx, right_idx, top_idx, bottom_idx)
+
+
+# image_stack = tifffile.imread(FILE_PATH)
+# print(image_stack.shape)
+# print(image_stack.dtype)
