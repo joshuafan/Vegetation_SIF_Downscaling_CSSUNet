292b22bb5777f4bc3b9c7fadfe7581d21efec32d
Joshua Fan <joshuayfan@hotmail.com>
2020-04-14 00:57:52 -0400

    Visualize

---
+++ visualize_dataset.py
@@ -0,0 +1,123 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import xarray as xr
+from sif_utils import plot_histogram
+
+# Taken from https://stackoverflow.com/questions/11159436/multiple-figures-in-a-single-window
+def plot_figures(output_file, figures, nrows = 1, ncols=1):
+    """Plot a dictionary of figures.
+
+    Parameters
+    ----------
+    figures : <title, figure> dictionary
+    ncols : number of columns of subplots wanted in the display
+    nrows : number of rows of subplots wanted in the figure
+    """
+    #fig = plt.figure(figsize=(8, 20))
+    fig, axeslist = plt.subplots(ncols=ncols, nrows=nrows, figsize=(20, 20))
+    for ind,title in enumerate(figures):
+        axeslist.ravel()[ind].imshow(figures[title])  #, cmap=plt.gray())
+        axeslist.ravel()[ind].set_title(title)
+        axeslist.ravel()[ind].set_axis_off()
+    plt.tight_layout() # optional
+    plt.savefig(output_file)
+    plt.close()
+
+def plot_images(image_rows, image_filename_column, output_file):
+    images = {}
+    for idx, image_row in image_rows.iterrows():
+        subtile = np.load(image_row[image_filename_column]).transpose((1, 2, 0))
+        title = 'Lat' + str(round(image_row['lat'], 6)) + ', Lon' + str(round(image_row['lon'], 6)) + ' (SIF = ' + str(round(image_row['SIF'], 3)) + ')'
+        images[title] = subtile[:, :, RGB_BANDS] / 1000
+    plot_figures(output_file, images, nrows=math.ceil(len(images) / 5), ncols=5)
+ 
+
+DATA_DIR = "/mnt/beegfs/bulk/mirror/jyf6/datasets"
+TILES_DIR = os.path.join(DATA_DIR, "tiles_2016-08-01")
+LAT_LON = 'lat_47.55_lon_-101.35'
+IMAGE_FILE = os.path.join(TILES_DIR, "reflectance_" + LAT_LON + ".npy")
+CFIS_SIF_FILE = os.path.join(DATA_DIR, "CFIS/CFIS_201608a_300m.npy")
+TROPOMI_SIF_FILE = os.path.join(DATA_DIR, "TROPOMI_SIF/TROPO-SIF_01deg_biweekly_Apr18-Jan20.nc")
+TROPOMI_DATE_RANGE = slice("2018-08-01", "2018-08-16")
+EVAL_SUBTILE_DATASET = os.path.join(DATA_DIR, "dataset_2016-08-01/eval_subtiles.csv")
+TRAIN_TILE_DATASET = os.path.join(DATA_DIR, "dataset_2018-08-01/tile_info_train.csv")
+RGB_BANDS = [3, 2, 1]
+
+# Display tiles with largest/smallest TROPOMI SIFs
+train_metadata = pd.read_csv(TRAIN_TILE_DATASET)
+highest_tropomi_sifs = train_metadata.nlargest(25, 'SIF')
+plot_images(highest_tropomi_sifs, 'tile_file', 'exploratory_plots/tropomi_sif_high_subtiles.png')
+lowest_tropomi_sifs = train_metadata.nsmallest(25, 'SIF')
+plot_images(lowest_tropomi_sifs, 'tile_file', 'exploratory_plots/tropomi_sif_low_subtiles.png')
+
+# Display tiles with largest/smallest CFIS SIFs
+eval_metadata = pd.read_csv(EVAL_SUBTILE_DATASET)
+highest_cfis_sifs = eval_metadata.nlargest(25, 'SIF')
+plot_images(highest_cfis_sifs, 'subtile_file', 'exploratory_plots/cfis_sif_high_subtiles.png')
+lowest_cfis_sifs = eval_metadata.nsmallest(25, 'SIF')
+plot_images(lowest_cfis_sifs, 'subtile_file', 'exploratory_plots/cfis_sif_low_subtiles.png')
+
+# Open CFIS SIF evaluation dataset
+#cfis_points = np.load(CFIS_SIF_FILE)
+#print("CFIS dataset shape", cfis_points.shape)
+print('Eval points', len(eval_metadata))
+
+# Open TROPOMI SIF dataset
+tropomi_dataset = xr.open_dataset(TROPOMI_SIF_FILE)
+tropomi_array = tropomi_dataset.sif_dc.sel(time=TROPOMI_DATE_RANGE).mean(dim='time')
+
+# For each CFIS SIF point, find TROPOMI SIF of surrounding tile
+tropomi_sifs = []  # TROPOMI SIF corresponding to each CFIS point
+for i in range(len(eval_metadata)):  # range(cfis_points.shape[0]):
+    lon = eval_metadata['lon'][i]  # cfis_points[i, 1]
+    lat = eval_metadata['lat'][i]  # cfis_points[i, 2]
+    tropomi_sif = tropomi_array.sel(lat=lat, lon=lon, method='nearest')
+    tropomi_sifs.append(tropomi_sif)
+
+# Plot histogram of CFIS and TROPOMI SIFs
+plot_histogram(np.array(eval_metadata['SIF']), "cfis_sif_distribution.png") #  cfis_points[:, 0])
+plot_histogram(np.array(tropomi_sifs), "tropomi_sif_distribution_validation_area.png")
+
+# Scatterplot of CFIS points
+green_cmap = plt.get_cmap('Greens')
+plt.scatter(eval_metadata['lon'], eval_metadata['lat'], c=eval_metadata['SIF'], cmap=green_cmap)
+# plt.scatter(cfis_points[:, 1], cfis_points[:, 2], c=cfis_points[:, 0], cmap=green_cmap)
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.title('Validation points')
+plt.savefig('exploratory_plots/validation_points_filtered.png')
+plt.close()
+
+# Plot TROPOMI vs SIF (and linear regression)
+x = eval_metadata['SIF']  # cfis_points[:, 0]
+y = tropomi_sifs
+coef = np.polyfit(x, y, 1)
+print('Linear regression: x=CFIS, y=TROPOMI', coef)
+poly1d_fn = np.poly1d(coef) 
+plt.plot(x, y, 'bo', x, poly1d_fn(x), '--k')
+plt.xlabel('CFIS SIF (small tile, 2016)')
+plt.ylabel('TROPOMI SIF (surrounding large tile, 2018)')
+plt.title('TROPOMI vs CFIS SIF')
+plt.savefig('exploratory_plots/TROPOMI_vs_CFIS_SIF')
+plt.close()
+
+# Show example tiles (RGB)
+array = np.load(IMAGE_FILE).transpose((1, 2, 0))
+print('Array shape', array.shape)
+plt.imshow(array[:, :, RGB_BANDS] / 1000)
+plt.savefig("exploratory_plots/dataset_rgb_2016.png")
+plt.close()
+
+fig, axeslist = plt.subplots(ncols=5, nrows=6, figsize=(20, 24))
+for band in range(0, 29):
+    layer = array[:, :, band]
+    axeslist.ravel()[band].imshow(layer, cmap='Greens', vmin=np.min(layer), vmax=np.max(layer))
+    axeslist.ravel()[band].set_title('Band ' + str(band))
+    axeslist.ravel()[band].set_axis_off()
+plt.tight_layout() # optional
+plt.savefig('exploratory_plots/dataset_' + LAT_LON +'.png')
+plt.close()
+
---
+++ visualize_eval_embeddings.py
@@ -0,0 +1,130 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import torch
+import sys
+sys.path.append('../')
+from tile2vec.src.tilenet import make_tilenet
+import tile_transforms
+import torchvision
+import torchvision.transforms as transforms
+from eval_subtile_dataset import EvalSubtileDataset
+
+DATA_DIR = "/mnt/beegfs/bulk/mirror/jyf6/datasets"
+EVAL_SUBTILE_DATASET_FILE = os.path.join(DATA_DIR, "dataset_2016-08-01/eval_subtiles.csv")
+TILE2VEC_MODEL_FILE = os.path.join(DATA_DIR, "models/tile2vec_dim256_neighborhood100/TileNet.ckpt")
+TRAIN_DATASET_DIR = os.path.join(DATA_DIR, "dataset_2018-08-01")
+BAND_STATISTICS_FILE = os.path.join(TRAIN_DATASET_DIR, "band_statistics_train.csv")
+
+eval_metadata = pd.read_csv(EVAL_SUBTILE_DATASET_FILE)
+Z_DIM = 256
+INPUT_CHANNELS = 43
+RGB_BANDS = [3, 2, 1]
+COVER_BANDS = list(range(12, 42))
+BATCH_SIZE = 4
+# Check if any CUDA devices are visible. If so, pick a default visible device.
+# If not, use CPU.
+if 'CUDA_VISIBLE_DEVICES' in os.environ:
+    print('CUDA_VISIBLE_DEVICES:', os.environ['CUDA_VISIBLE_DEVICES'])
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+else:
+    device = "cpu"
+print("Device", device)
+
+# Read mean/standard deviation for each band, for standardization purposes
+train_statistics = pd.read_csv(BAND_STATISTICS_FILE)
+train_means = train_statistics['mean'].values
+train_stds = train_statistics['std'].values
+print("Eval samples", len(eval_metadata))
+print("Means", train_means)
+print("Stds", train_stds)
+band_means = train_means[:-1]
+sif_mean = train_means[-1]
+band_stds = train_stds[:-1] * 371
+sif_std = train_stds[-1]
+
+# Set up image transforms
+transform_list = []
+transform_list.append(tile_transforms.StandardizeTile(band_means, band_stds))
+transform = transforms.Compose(transform_list)
+
+# Set up Dataset and Dataloader
+dataset_size = len(eval_metadata)
+dataset = EvalSubtileDataset(eval_metadata, transform=transform)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE,
+                                         shuffle=True, num_workers=4)
+
+# Load Tile2Vec model
+tile2vec_model = make_tilenet(in_channels=INPUT_CHANNELS, z_dim=Z_DIM)
+tile2vec_model.load_state_dict(torch.load(TILE2VEC_MODEL_FILE, map_location=device))
+
+# Calculate all subtile embeddings
+subtile_embeddings = np.zeros((2000, Z_DIM))  # (len(eval_dataset)z_dim))
+i = 0
+for sample in dataloader:
+    input_tile_standardized = sample['subtile'].to(device)
+    batch = input_tile_standardized.shape[0]
+    with torch.set_grad_enabled(False):
+        subtile_embeddings[i:i+batch] = tile2vec_model(input_tile_standardized)
+    i += batch
+    if i >= 2000:
+        break
+
+# Loads tile from file and transforms it into the format that imshow wants
+def tile_to_image(tile):
+    tile = tile.transpose((1, 2, 0))
+    return tile[:, :, RGB_BANDS] / 1000
+
+def get_title_string(image_row):
+    return 'Lat' + str(round(image_row['lat'], 6)) + ', Lon' + str(round(image_row['lon'], 6)) + ' (SIF = ' + str(round(image_row['SIF'], 3)) + ')'
+
+
+NUM_NEIGHBORS = 5
+
+for anchor_idx in range(5):
+    distances_to_anchor = np.zeros((subtile_embeddings.shape[0]))
+    for i in range(subtile_embeddings.shape[0]):
+        distances_to_anchor[i] = np.linalg.norm(subtile_embeddings[anchor_idx] - subtile_embeddings[i])
+    sorted_indices = distances_to_anchor.argsort()  # Ascending order of distance
+    closest_indices = sorted_indices[:NUM_NEIGHBORS]
+    furthest_indices = sorted_indices[-NUM_NEIGHBORS:][::-1]
+    print('Closest indices', closest_indices)
+    closest_subtile_rows = eval_metadata.iloc[closest_indices]
+    furthest_subtile_rows = eval_metadata.iloc[furthest_indices]
+    anchor_subtile_row = eval_metadata.iloc[anchor_idx]
+
+    fig, axeslist = plt.subplots(ncols=NUM_NEIGHBORS, nrows=3, figsize=(30, 30))
+
+    # Display anchor subtile
+    anchor_tile = np.load(anchor_subtile_row['subtile_file'])
+    axeslist[0][0].imshow(tile_to_image(anchor_tile))
+    axeslist[0][0].set_title('Anchor tile: ' + get_title_string(anchor_subtile_row))
+    axeslist[0][0].set_axis_off()
+    print('Anchor tile crops', anchor_tile[COVER_BANDS, :, :].mean(axis=(1,2)))
+
+    # display closest subtiles
+    i = 0
+    for idx, close_subtile_row in closest_subtile_rows.iterrows():
+        close_tile = np.load(close_subtile_row['subtile_file'])
+        axeslist[1][i].imshow(tile_to_image(close_tile))
+        axeslist[1][i].set_title('close: ' + get_title_string(close_subtile_row))
+        axeslist[1][i].set_axis_off()
+        print('close tile crops', close_tile[COVER_BANDS, :, :].mean(axis=(1,2)))
+        i += 1
+
+    # display closest subtiles
+    i = 0
+    for idx, far_subtile_row in furthest_subtile_rows.iterrows():
+        far_tile = np.load(far_subtile_row['subtile_file'])
+        axeslist[2][i].imshow(tile_to_image(far_tile))
+        axeslist[2][i].set_title('far: ' + get_title_string(far_subtile_row))
+        axeslist[2][i].set_axis_off()
+        print('far tile crops', far_tile[COVER_BANDS, :, :].mean(axis=(1,2)))
+        i += 1
+
+    plt.savefig('exploratory_plots/embedding_neighbors_' + os.path.basename(anchor_subtile_row['subtile_file']) + '.png')
+    plt.close()
+   
+
+
