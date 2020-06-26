4eb89c08cf1f88579c34bd3e4e545b1d93530d8a
Joshua Fan <joshuayfan@hotmail.com>
2020-06-26 16:30:24 -0400

    Updates to simple CNN

--- simple_cnn.py
+++ simple_cnn.py
@@ -44,18 +44,30 @@ class SimpleCNN(nn.Module):
         return x
     
 class SimpleCNNSmall(nn.Module):
-    def __init__(self, input_channels, output_dim, reduced_channels=15, min_output=None, max_output=None):
+    def __init__(self, input_channels, output_dim, crop_type_start_idx, crop_type_embedding_dim=15, reduced_channels=20, min_output=None, max_output=None):
         super(SimpleCNNSmall, self).__init__()
-        self.dimensionality_reduction = nn.Conv2d(input_channels, reduced_channels, kernel_size=1, stride=1)
-        self.conv1 = nn.Conv2d(reduced_channels, 8, kernel_size=3, stride=1)
+
+        # Create list of crop type indices and non-crop indices
+        self.non_crop_type_bands = list(range(0, crop_type_start_idx)) + [input_channels - 1]  # Last channel is the "missing reflectance" mask and is not a crop type
+        self.crop_type_bands = list(range(crop_type_start_idx, input_channels - 1))
+        # print('non crop type indices', self.non_crop_type_bands)
+        # print('crop type indices', self.crop_type_bands)
+
+        # # Embedding vector for each crop type's pixels
+        # self.crop_type_embedding = nn.Conv2d(len(self.crop_type_bands), crop_type_embedding_dim, kernel_size=1, stride=1)
+        
+        # # Embed each pixel. Each pixel's vector should contain semantic information about
+        # # the crop type + reflectance + other features
+        # channels_after_embedding = input_channels - len(self.crop_type_bands) + crop_type_embedding_dim  # Number of features after embedding crop type
+        # print('Channels after embedding', channels_after_embedding)
+
+        self.dimensionality_reduction_1 = nn.Conv2d(input_channels, reduced_channels, kernel_size=1, stride=1)
+        self.dimensionality_reduction_2 = nn.Conv2d(reduced_channels, reduced_channels, kernel_size=1, stride=1)
+        self.conv1 = nn.Conv2d(reduced_channels, 64, kernel_size=3, stride=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=2, stride=2)
         self.pool = nn.AvgPool2d(2, 2)
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, stride=1)
-        self.fc1 = nn.Linear(16 * 2 * 2, 16)
-        #self.conv2 = nn.Conv2d(64, 128, kernel_size=2, stride=1)
-        #self.conv3 = nn.Conv2d(128, 256, kernel_size=2, stride=1)
-        #self.fc1 = nn.Linear(256 * 2 * 2, 256)
-        #self.fc2 = nn.Linear(64, 16)
-        self.fc3 = nn.Linear(16, output_dim)
+        self.fc1 = nn.Linear(128, 64)
+        self.fc2 = nn.Linear(64, output_dim)
 
         if min_output is not None and max_output is not None:
             self.restrict_output = True
@@ -65,19 +77,33 @@ class SimpleCNNSmall(nn.Module):
             self.restrict_output = False
 
     def forward(self, x):
-        x = F.relu(self.dimensionality_reduction(x))
+        # crop_masks = x[:, self.crop_type_bands, :, :]
+        # print('===================================================')
+        # print('Random pixel (7, 7)', x[0, :, 7, 7])
+        # print('Random pixel (7, 8)', x[0, :, 7, 8])
+        # print('Random pixel (7, 9)', x[0, :, 7, 9])
+        # print('Crop masks shape', crop_masks.shape)
+        # crop_embeddings = self.crop_type_embedding(crop_masks)
+        # print('Crop embeddings shape', crop_embeddings.shape)
+        # x = torch.cat([x[:, self.non_crop_type_bands, :, :], crop_embeddings], dim=1)
+
+        # print('After stacking', x.shape)
+        # print('(After embedding) Random pixel (7, 7)', x[0, :, 7, 7])
+        # print('Random pixel (7, 8)', x[0, :, 7, 8])
+        # print('Random pixel (7, 9)', x[0, :, 7, 9])
+
+        # Dimensionality reduction (1x1 convolution)
+        x = F.relu(self.dimensionality_reduction_1(x))
+        x = F.relu(self.dimensionality_reduction_2(x))
+
+        # Convolutional layers
         x = self.pool(F.relu(self.conv1(x)))
-        #print('After layer 1', x.shape)
-        x = F.relu(self.conv2(x))
-        #x = F.relu(self.conv3(x))
-        #x = self.pool(F.relu(self.conv2(x)))
-        #print('After layer 2', x.shape)
-        x = x.view(-1, 16 * 2 * 2)
-        #x = x.view(-1, 256 * 2 * 2)
-        #print('Before fully-connected', x.shape)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 128 * 1 * 1)
+
+        # Fully-connected layers
         x = F.relu(self.fc1(x))
-        #x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
         if self.restrict_output:
             x = (F.tanh(x) * self.scale_factor) + self.mean_output
             #x = torch.clamp(x, min=self.min_output, max=self.max_output)
