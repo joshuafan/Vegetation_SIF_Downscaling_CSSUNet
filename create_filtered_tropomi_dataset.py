Fix dataset filtering, calculate reflectance averages only over non-missing

create_filtered_tropomi_dataset.py
@@ -6,18 +6,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
+import pickle
+import random
 import sklearn.model_selection
-from sif_utils import plot_histogram
+from sif_utils import plot_histogram, determine_split
 import torch
 
 DATA_DIR = "/mnt/beegfs/bulk/mirror/jyf6/datasets"
-DATASET_DIR = os.path.join(DATA_DIR, "dataset_2018-08-01")
-UNFILTERED_CSV_FILES = {"train": os.path.join(DATASET_DIR, "tile_info_unfiltered_train.csv"),
-                        "val": os.path.join(DATASET_DIR, "tile_info_unfiltered_val.csv")}
-FILTERED_CSV_FILES = {"train": os.path.join(DATASET_DIR, "tile_info_train.csv"),
-                      "val": os.path.join(DATASET_DIR, "tile_info_val.csv")}
-BAND_STATISTICS_CSV_FILES = {"train": os.path.join(DATASET_DIR, "band_statistics_train.csv"),
-                             "val": os.path.join(DATASET_DIR, "band_statistics_val.csv")}
+DATES = ["2018-07-08", "2018-07-22", "2018-08-05", "2018-08-19", "2018-09-02"]
+DATASET_DIRS = [os.path.join(DATA_DIR, "dataset_" + date) for date in DATES]
+UNFILTERED_TROPOMI_FILES = [os.path.join(dataset_dir, "tile_averages.csv") for dataset_dir in DATASET_DIRS]
+UNFILTERED_OCO2_FILES = [os.path.join(dataset_dir, "oco2_eval_subtiles.csv") for dataset_dir in DATASET_DIRS]
+# print("Dataset dirs", DATASET_DIRS)
+# print("Unfiltered Tropomi files:", UNFILTERED_TROPOMI_FILES)
+# print("Unfiltered OCO2 files:", UNFILTERED_OCO2_FILES)
+
+# Create a folder for the processed dataset
+PROCESSED_DATASET_DIR = os.path.join(DATA_DIR, "processed_dataset")
+if not os.path.exists(PROCESSED_DATASET_DIR):
+    os.makedirs(PROCESSED_DATASET_DIR)
+
+# Record the split of large grid areas between train/val/test
+TRAIN_VAL_TEST_SPLIT_FILE = os.path.join(PROCESSED_DATASET_DIR, "data_split.csv")
+
+# Resulting filtered csv files
+FILTERED_CSV_FILES = {"train": os.path.join(PROCESSED_DATASET_DIR, "tile_info_train.csv"),
+                      "val": os.path.join(PROCESSED_DATASET_DIR, "tile_info_val.csv"),
+                      "test": os.path.join(PROCESSED_DATASET_DIR, "tile_info_test.csv")}
+BAND_STATISTICS_CSV_FILES = {"train": os.path.join(PROCESSED_DATASET_DIR, "band_statistics_train.csv"),
+                             "val": os.path.join(PROCESSED_DATASET_DIR, "band_statistics_val.csv"),
+                             "test": os.path.join(PROCESSED_DATASET_DIR, "band_statistics_test.csv")}
 
 STATISTICS_COLUMNS = ['ref_1', 'ref_2', 'ref_3', 'ref_4', 'ref_5', 'ref_6', 'ref_7',
                     'ref_10', 'ref_11', 'Rainf_f_tavg', 'SWdown_f_tavg', 'Tair_f_tavg', 
@@ -30,43 +48,127 @@ STATISTICS_COLUMNS = ['ref_1', 'ref_2', 'ref_3', 'ref_4', 'ref_5', 'ref_6', 'ref
                     'millet', 'sugarbeets', 'oats', 'mixed_forest', 'peas', 'barley',
                     'lentils', 'missing_reflectance', 'SIF']
 
-MAX_LANDSAT_CLOUD_COVER = 0.1
+MAX_LANDSAT_CLOUD_COVER = 0.2
 MAX_TROPOMI_CLOUD_COVER = 0.2
-MIN_TROPOMI_N = 5
+MIN_TROPOMI_NUM_SOUNDINGS = 5
+MIN_OCO2_NUM_SOUNDINGS = 3
 MIN_SIF = 0.2
+FRACTION_VAL = 0.2
+FRACTION_TEST = 0.2
+OCO2_SCALING_FACTOR = 1.69
 
-for split in ["train", "val"]:
-    tile_metadata = pd.read_csv(UNFILTERED_CSV_FILES[split])
-    print('Split:', split)
-    print('Number of tiles:', len(tile_metadata))
+# Divide the region into 1x1 degree large grid areas. Split them between train/val/test
+LONS = list(range(-108, -82))  # These lat/lons are the UPPER LEFT corner of the large grid areas
+LATS = list(range(39, 50))
+large_grid_areas = dict()
+for lon in LONS:
+    for lat in LATS:
+        random_number = random.random()
+        if random_number < 1 - FRACTION_TEST - FRACTION_VAL:
+            split = 'train'
+        elif random_number < 1 - FRACTION_TEST:
+            split = 'val'
+        else:
+            split = 'test'
+        large_grid_areas[(lon, lat)] = split
 
-    # Restrict to only tiles where TROPOMI (SIF) cloud cover is low
-    tile_metadata = tile_metadata.loc[tile_metadata['cloud_fraction'] < MAX_TROPOMI_CLOUD_COVER]
-    print('After filtering cloudy TROPOMI SIF:', len(tile_metadata))  
+# Save the split to a file
+with open(TRAIN_VAL_TEST_SPLIT_FILE, 'wb') as f:
+    pickle.dump(large_grid_areas, f, pickle.HIGHEST_PROTOCOL)
 
-    # Restrict to only tiles where TROPOMI has enough soundings
-    tile_metadata = tile_metadata.loc[tile_metadata['num_soundings'] >= MIN_TROPOMI_N]
-    print('After filtering low # of soundings:', len(tile_metadata))
+# Read TROPOMI files
+tropomi_frames = []
+for info_file in UNFILTERED_TROPOMI_FILES:
+    tropomi_frame = pd.read_csv(info_file) 
+    tropomi_frame['source'] = 'TROPOMI'
+    tropomi_frames.append(tropomi_frame)
+tropomi_metadata = pd.concat(tropomi_frames)
+print("TROPOMI average SIF", tropomi_metadata['SIF'].mean())
+print('TROPOMI tiles before filter', len(tropomi_metadata))
 
-    # Restrict to only tiles where SIF is at least 0.2 (low SIF tiles are noisy)
-    tile_metadata = tile_metadata.loc[tile_metadata['SIF'] >= MIN_SIF]
-    print('After filtering low SIF:', len(tile_metadata))
+# Restrict to only tiles where Landsat (reflectance) cloud cover is low
+tropomi_metadata = tropomi_metadata.loc[tropomi_metadata['missing_reflectance'] <= MAX_LANDSAT_CLOUD_COVER]
+print('TROPOMI tiles after filtering missing reflectance:', len(tropomi_metadata))
 
-    # Restrict to only tiles where Landsat (reflectance) cloud cover is low
-    tile_metadata = tile_metadata.loc[tile_metadata['missing_reflectance'] < MAX_LANDSAT_CLOUD_COVER]
-    print('After filtering missing reflectance:', len(tile_metadata))
+# Restrict to only tiles where TROPOMI (SIF) cloud cover is low
+tropomi_metadata = tropomi_metadata.loc[tropomi_metadata['cloud_fraction'] <= MAX_TROPOMI_CLOUD_COVER]
+print('TROPOMI tiles after filtering cloudy TROPOMI SIF:', len(tropomi_metadata))  
 
-    # Write filtered dataset to csv file
-    tile_metadata.reset_index(drop=True, inplace=True)
-    tile_metadata.to_csv(FILTERED_CSV_FILES[split])
+# Restrict to only tiles where TROPOMI has enough soundings
+tropomi_metadata = tropomi_metadata.loc[tropomi_metadata['num_soundings'] >= MIN_TROPOMI_NUM_SOUNDINGS]
+print('TROPOMI tiles after filtering low # of soundings:', len(tropomi_metadata))
+
+# Restrict to only tiles where SIF is at least 0.2 (low SIF tiles are noisy)
+tropomi_metadata = tropomi_metadata.loc[tropomi_metadata['SIF'] >= MIN_SIF]
+print('TROPOMI tiles after filtering low SIF:', len(tropomi_metadata))
+
+
+# Read OCO-2 files
+oco2_frames = []
+for info_file in UNFILTERED_OCO2_FILES:
+    oco2_frame = pd.read_csv(info_file) 
+    oco2_frame['source'] = 'OCO2'
+    oco2_frame['SIF'] *= OCO2_SCALING_FACTOR  # OCO-2 SIF needs to be scaled to match TROPOMI
+    oco2_frames.append(oco2_frame)
+oco2_metadata = pd.concat(oco2_frames)
+print("OCO2 average SIF", oco2_metadata['SIF'].mean())
+print("OCO2 tiles before filter", len(oco2_metadata))
+
+# Restrict to only tiles where Landsat (reflectance) cloud cover is low
+oco2_metadata = oco2_metadata.loc[oco2_metadata['missing_reflectance'] <= MAX_LANDSAT_CLOUD_COVER]
+print('OCO2 tiles after filtering missing reflectance:', len(oco2_metadata))
+
+# Restrict to only tiles where OCO-2 has enough soundings
+oco2_metadata = oco2_metadata.loc[oco2_metadata['num_soundings'] >= MIN_OCO2_NUM_SOUNDINGS]
+print('OCO2 tiles after filtering low # of soundings:', len(oco2_metadata))
+
+# Restrict to only tiles where SIF is at least 0.2 (low SIF tiles are noisy)
+oco2_metadata = oco2_metadata.loc[oco2_metadata['SIF'] >= MIN_SIF]
+print('OCO2 tiles after filtering low SIF:', len(oco2_metadata))
+
+
+# Combine OCO2 and TROPOMI tiles into a single dataframe
+tile_metadata = pd.concat([tropomi_metadata, oco2_metadata])
+tile_metadata.reset_index(drop=True, inplace=True)
+
+
+# Shuffle rows to mix OCO2/TROPOMI together
+tile_metadata = tile_metadata.sample(frac=1).reset_index(drop=True)
+print('After all filtering:', tile_metadata['source'].value_counts())
+print('TROPOMI by date', tropomi_metadata['date'].value_counts())
+print('OCO2 by date', oco2_metadata['date'].value_counts())
+
+# For each row, determine whether it's in a train, val, or test large grid region
+tile_metadata['split'] = tile_metadata.apply(lambda row: determine_split(large_grid_areas, row), axis=1)
+
+split_metadata = {'train': tile_metadata[tile_metadata['split'] == 'train'],
+                  'val': tile_metadata[tile_metadata['split'] == 'val'],
+                  'test': tile_metadata[tile_metadata['split'] == 'test']}
+
+# For each split, write the processed dataset to a file
+for split, metadata in split_metadata.items():
+    print('Split', split, 'Number of rows', len(metadata))
+    metadata.reset_index(drop=True, inplace=True)
+    metadata.to_csv(FILTERED_CSV_FILES[split])
+
+    # Number of pure pixels
+    pure_grassland_pasture = metadata.loc[(metadata['grassland_pasture'] > 0.6) & (metadata['source'] == 'OCO2')]
+    pure_corn = metadata.loc[(metadata['corn'] > 0.6) & (metadata['source'] == 'OCO2')]
+    pure_soybean = metadata.loc[(metadata['soybean'] > 0.6) & (metadata['source'] == 'OCO2')]
+    pure_deciduous_forest = metadata.loc[(metadata['deciduous_forest'] > 0.6) & (metadata['source'] == 'OCO2')]
+    print('===================== Split', split, '=====================')
+    print("OCO2 pure grassland/pasture", len(pure_grassland_pasture))
+    print("OCO2 pure corn", len(pure_corn))
+    print("OCO2 pure soybean", len(pure_soybean))
+    print("OCO2 pure deciduous forest", len(pure_deciduous_forest))
 
     # Compute averages for each band
-    selected_columns = tile_metadata[STATISTICS_COLUMNS]
-    print("Band values ARRAY shape", selected_columns.shape)
+    selected_columns = metadata[STATISTICS_COLUMNS]
+    # print("Band values ARRAY shape", selected_columns.shape)
     band_means = selected_columns.mean(axis=0)
     band_stds = selected_columns.std(axis=0)
     print("Band means", band_means)
-    print("Band stds", band_stds)
+    # print("Band stds", band_stds)
 
     # Write band averages to file
     statistics_rows = [['mean', 'std']]

create_oco2_eval_dataset.py
@@ -12,19 +12,19 @@ from sif_utils import plot_histogram, lat_long_to_index
 
 DATA_DIR = "/mnt/beegfs/bulk/mirror/jyf6/datasets"
 #OCO2_FILE = os.path.join(DATA_DIR, "OCO2/oco2_20180801_20180816_1800m.nc")
-OCO2_FILE = os.path.join(DATA_DIR, "OCO2/oco2_20180801_20180816_3km.nc")
-DATE = "2018-08-01" #"2016-07-16"
-TILES_DIR = os.path.join(DATA_DIR, "tiles_" + DATE)
-DATASET_DIR = os.path.join(DATA_DIR, "dataset_" + DATE)
-OCO2_SUBTILES_DIR = os.path.join(DATA_DIR, "oco2_subtiles_" + DATE)  # Directory to output subtiles to
-OUTPUT_CSV_FILE = os.path.join(DATASET_DIR, "oco2_eval_subtiles.csv")
+#OCO2_FILE = os.path.join(DATA_DIR, "OCO2/oco2_20180801_20180816_3km.nc")
+OCO2_FILE = os.path.join(DATA_DIR, "OCO2/oco2_20180708_20180915_14day_3km.nc")
+#DATE = "2018-08-01" #"2016-07-16"
+# TILES_DIR = os.path.join(DATA_DIR, "tiles_" + DATE)
+# DATASET_DIR = os.path.join(DATA_DIR, "dataset_" + DATE)
+#OCO2_SUBTILES_DIR = os.path.join(DATA_DIR, "oco2_subtiles_" + DATE)  # Directory to output subtiles to
+#OUTPUT_CSV_FILE = os.path.join(DATASET_DIR, "oco2_eval_subtiles.csv")
+OUTPUT_CSV_FILENAME = "oco2_eval_subtiles.csv"
+# if not os.path.exists(OCO2_SUBTILES_DIR):
+#     os.makedirs(OCO2_SUBTILES_DIR)
 
-if not os.path.exists(OCO2_SUBTILES_DIR):
-    os.makedirs(OCO2_SUBTILES_DIR)
-
-dataset_rows = []
-dataset_rows.append(["lon", "lat", "date",
-                     "subtile_file", 'ref_1', 'ref_2', 'ref_3', 'ref_4', 'ref_5', 'ref_6', 'ref_7',
+COLUMN_NAMES = ["lon", "lat", "date",
+                     "tile_file", 'ref_1', 'ref_2', 'ref_3', 'ref_4', 'ref_5', 'ref_6', 'ref_7',
                     'ref_10', 'ref_11', 'Rainf_f_tavg', 'SWdown_f_tavg', 'Tair_f_tavg', 
                     'grassland_pasture', 'corn', 'soybean', 'shrubland',
                     'deciduous_forest', 'evergreen_forest', 'spring_wheat', 'developed_open_space',
@@ -33,7 +33,7 @@ dataset_rows.append(["lon", "lat", "date",
                     'sorghum', 'developed_low_intensity', 'barren', 'durum_wheat',
                     'canola', 'sunflower', 'dry_beans', 'developed_med_intensity',
                     'millet', 'sugarbeets', 'oats', 'mixed_forest', 'peas', 'barley',
-                    'lentils', 'missing_reflectance', "SIF", "num_soundings"])
+                    'lentils', 'missing_reflectance', "SIF", "num_soundings"]
 
 
 
@@ -41,6 +41,8 @@ dataset_rows.append(["lon", "lat", "date",
 point_lons = []
 point_lats = []
 point_sifs = []
+
+# Parameters
 RES = (0.00026949458523585647, 0.00026949458523585647)
 LARGE_TILE_PIXELS = 371
 SUBTILE_PIXELS = 100
@@ -53,20 +55,22 @@ INPUT_CHANNELS = 43
 CDL_BANDS = list(range(12, 42))
 MIN_CDL_COVERAGE = 0.5
 
-
 pure_corn_points = 0
 pure_soy_points = 0
 missing_points = 0
 
+# Open dataset and print
 dataset = xr.open_dataset(OCO2_FILE)
 print(dataset)
-dataset = dataset.mean('time')
+
+times = dataset.time.values
 lons = dataset.lon.values
 lats = dataset.lat.values
 sifs = dataset.dcSIF.values
 num_soundings = dataset.n.values
 
 plot_histogram(sifs, "sif_distribution_oco2_all.png", title="OCO2 SIF distribution (longitude: -108 to -82, latitude: 38 to 48.7)")
+print('Times', times.shape)
 print('Lons', lons.shape)
 print('Lats', lats.shape)
 print('Sifs', sifs.shape)
@@ -83,145 +87,168 @@ points_missing_reflectance = 0  # Number of points with missing reflectance data
 points_with_reflectance = 0  # Number of points with reflectance data
 
 # Loop through all OCO-2 data points
-for lon_idx in range(len(lons)):
-    for lat_idx in range(len(lats)):
-        lon = lons[lon_idx]
-        lat = lats[lat_idx]        
-        sif = sifs[lat_idx, lon_idx]
-        num_soundings_point = num_soundings[lat_idx, lon_idx]
+# Loop through all time ranges
+datapoints = 0
+for time_idx in range(len(times)):
+    date_time = times[time_idx]
+    DATE = np.datetime_as_string(date_time, unit='D')
+    print('Date', DATE)
 
-        if math.isnan(sif):
-            continue
+    TILES_DIR = os.path.join(DATA_DIR, "tiles_" + DATE)
+    DATASET_DIR = os.path.join(DATA_DIR, "dataset_" + DATE)  
+    OCO2_SUBTILES_DIR = os.path.join(DATA_DIR, "oco2_subtiles_" + DATE)
 
-        # If SIF is low, we don't care about this point / it's likely to be noisy, so ignore it
-        # if sif < MIN_SIF:
-        #     continue
-        
-        # if num_soundings_point < MIN_NUM_SOUNDINGS:
-        #     continue
-         
-        # sifs_to_average = []
-        # sif_lat_idx_low = max(0, lat_idx - 1)
-        # sif_lat_idx_high = min(sifs.shape[0], lat_idx + 2) # Exclusive (high lat index)
-        # sif_lon_idx_low = max(0, lon_idx - 1)
-        # sif_lon_idx_high = min(sifs.shape[1], lon_idx + 2)
-        # print('Lon idx', sif_lon_idx_low, sif_lon_idx_high)
-        # print('Lat idx', sif_lat_idx_low, sif_lat_idx_high)
-        # for i in range(sif_lat_idx_low, sif_lat_idx_high):
-        #     for j in range(sif_lon_idx_low, sif_lon_idx_high):
-        #         neighbor_sif = sifs[i, j]
-        #         if not math.isnan(neighbor_sif):
-        #             sifs_to_average.append(neighbor_sif)
-        # print('Centre', sifs[lat_idx, lon_idx])
-        # print('SIFs to average', sifs_to_average)
-        # sif = sum(sifs_to_average) / len(sifs_to_average)
+    # Make directory for OCO2 subtiles, if it does not exist
+    if not os.path.exists(OCO2_SUBTILES_DIR):
+        os.makedirs(OCO2_SUBTILES_DIR)
+    
+    # Initialize this time period's dataset of OCO2 values
+    dataset_rows = []
+    dataset_rows.append(COLUMN_NAMES)
 
-        # Compute rectangle that bounds this OCO-2 observation
-        min_lon = lon - SUBTILE_DEGREES / 2
-        max_lon = lon + SUBTILE_DEGREES / 2
-        min_lat = lat - SUBTILE_DEGREES / 2
-        max_lat = lat + SUBTILE_DEGREES / 2
-        print('====================================')
-        print("Lon: min", min_lon, "max:", max_lon)
-        print("Lat: min", min_lat, "max:", max_lat)
+    # Loop through all lat/lon
+    for lon_idx in range(len(lons)):
+        for lat_idx in range(len(lats)):
+            lon = lons[lon_idx]
+            lat = lats[lat_idx]        
+            sif = sifs[lat_idx, lon_idx, time_idx]
+            num_soundings_point = num_soundings[lat_idx, lon_idx, time_idx]
 
-        # Figure out which reflectance files to open. For each edge of the bounding box,
-        # find the left/top bound of the surrounding reflectance large tile.
-        min_lon_tile_left = (math.floor(min_lon * 10) / 10)
-        max_lon_tile_left = (math.floor(max_lon * 10) / 10)
-        min_lat_tile_top = (math.ceil(min_lat * 10) / 10)
-        max_lat_tile_top = (math.ceil(max_lat * 10) / 10)
-        num_tiles_lon = round((max_lon_tile_left - min_lon_tile_left) * 10) + 1
-        num_tiles_lat = round((max_lat_tile_top - min_lat_tile_top) * 10) + 1
-        file_left_lons = np.linspace(min_lon_tile_left, max_lon_tile_left, num_tiles_lon, endpoint=True)
-        file_top_lats = np.linspace(min_lat_tile_top, max_lat_tile_top, num_tiles_lat, endpoint=True)[::-1]  # Go through lats from top to bottom, because indices are numbered from top to bottom
-        print("File left lons", file_left_lons)
-        print("File top lats", file_top_lats)
+            # Ignore grid squares where there is no data
+            if math.isnan(sif):
+                continue
 
-        # Because a sub-tile could span multiple files, patch all of the files together
-        columns = []
-        for file_left_lon in file_left_lons:
-            rows = []
-            for file_top_lat in file_top_lats:
-                # Find what reflectance file to read from
-                file_center_lon = round(file_left_lon + 0.05, 2)
-                file_center_lat = round(file_top_lat - 0.05, 2)
-                large_tile_filename = TILES_DIR + "/reflectance_lat_" + str(file_center_lat) + "_lon_" + str(file_center_lon) + ".npy"
-                if not os.path.exists(large_tile_filename):
-                    print('Needed data file', large_tile_filename, 'does not exist!')
-                    # For now, consider the data for this section as missing
-                    missing_tile = np.zeros((INPUT_CHANNELS, LARGE_TILE_PIXELS, LARGE_TILE_PIXELS))
-                    missing_tile[-1, :, :] = 1
-                    rows.append(missing_tile)
-                else:
-                    print('Large tile filename', large_tile_filename)
-                    large_tile = np.load(large_tile_filename)
-                    rows.append(large_tile)
+            # If SIF is low, we don't care about this point / it's likely to be noisy, so ignore it
+            # if sif < MIN_SIF:
+            #     continue
+            
+            # if num_soundings_point < MIN_NUM_SOUNDINGS:
+            #     continue
+            
+            # sifs_to_average = []
+            # sif_lat_idx_low = max(0, lat_idx - 1)
+            # sif_lat_idx_high = min(sifs.shape[0], lat_idx + 2) # Exclusive (high lat index)
+            # sif_lon_idx_low = max(0, lon_idx - 1)
+            # sif_lon_idx_high = min(sifs.shape[1], lon_idx + 2)
+            # print('Lon idx', sif_lon_idx_low, sif_lon_idx_high)
+            # print('Lat idx', sif_lat_idx_low, sif_lat_idx_high)
+            # for i in range(sif_lat_idx_low, sif_lat_idx_high):
+            #     for j in range(sif_lon_idx_low, sif_lon_idx_high):
+            #         neighbor_sif = sifs[i, j]
+            #         if not math.isnan(neighbor_sif):
+            #             sifs_to_average.append(neighbor_sif)
+            # print('Centre', sifs[lat_idx, lon_idx])
+            # print('SIFs to average', sifs_to_average)
+            # sif = sum(sifs_to_average) / len(sifs_to_average)
 
-            column = np.concatenate(rows, axis=1)
-            columns.append(column)
-        
-        combined_large_tiles = np.concatenate(columns, axis=2)
-        print('All large tiles shape', combined_large_tiles.shape)
+            # Compute rectangle that bounds this OCO-2 observation
+            min_lon = lon - SUBTILE_DEGREES / 2
+            max_lon = lon + SUBTILE_DEGREES / 2
+            min_lat = lat - SUBTILE_DEGREES / 2
+            max_lat = lat + SUBTILE_DEGREES / 2
+            print('====================================')
+            print("Lon: min", min_lon, "max:", max_lon)
+            print("Lat: min", min_lat, "max:", max_lat)
 
-        # Find indices of bounding box within this combined large tile 
-        top_idx, left_idx = lat_long_to_index(max_lat, min_lon, max_lat_tile_top, min_lon_tile_left, RES)
-        bottom_idx = top_idx + SUBTILE_PIXELS
-        right_idx = left_idx + SUBTILE_PIXELS
-        print('From combined large tile: Top', top_idx, 'Botom', bottom_idx, 'Left', left_idx, 'Right', right_idx)
+            # Figure out which reflectance files to open. For each edge of the bounding box,
+            # find the left/top bound of the surrounding reflectance large tile.
+            min_lon_tile_left = (math.floor(min_lon * 10) / 10)
+            max_lon_tile_left = (math.floor(max_lon * 10) / 10)
+            min_lat_tile_top = (math.ceil(min_lat * 10) / 10)
+            max_lat_tile_top = (math.ceil(max_lat * 10) / 10)
+            num_tiles_lon = round((max_lon_tile_left - min_lon_tile_left) * 10) + 1
+            num_tiles_lat = round((max_lat_tile_top - min_lat_tile_top) * 10) + 1
+            file_left_lons = np.linspace(min_lon_tile_left, max_lon_tile_left, num_tiles_lon, endpoint=True)
+            file_top_lats = np.linspace(min_lat_tile_top, max_lat_tile_top, num_tiles_lat, endpoint=True)[::-1]  # Go through lats from top to bottom, because indices are numbered from top to bottom
+            print("File left lons", file_left_lons)
+            print("File top lats", file_top_lats)
 
-        # If the selected region (box) goes outside the range of the cover or reflectance dataset, that's a bug!
-        if top_idx < 0 or left_idx < 0:
-            print("Index was negative!")
-            exit(1)
-        if (bottom_idx >= combined_large_tiles.shape[1] or right_idx >= combined_large_tiles.shape[2]):
-            print("Reflectance index went beyond edge of array!")
-            exit(1)
+            # Because a sub-tile could span multiple files, patch all of the files together
+            columns = []
+            for file_left_lon in file_left_lons:
+                rows = []
+                for file_top_lat in file_top_lats:
+                    # Find what reflectance file to read from
+                    file_center_lon = round(file_left_lon + 0.05, 2)
+                    file_center_lat = round(file_top_lat - 0.05, 2)
+                    large_tile_filename = TILES_DIR + "/reflectance_lat_" + str(file_center_lat) + "_lon_" + str(file_center_lon) + ".npy"
+                    if not os.path.exists(large_tile_filename):
+                        print('Needed data file', large_tile_filename, 'does not exist!')
+                        # For now, consider the data for this section as missing
+                        missing_tile = np.zeros((INPUT_CHANNELS, LARGE_TILE_PIXELS, LARGE_TILE_PIXELS))
+                        missing_tile[-1, :, :] = 1
+                        rows.append(missing_tile)
+                    else:
+                        print('Large tile filename', large_tile_filename)
+                        large_tile = np.load(large_tile_filename)
+                        rows.append(large_tile)
 
-        subtile = combined_large_tiles[:, top_idx:bottom_idx, left_idx:right_idx]
+                column = np.concatenate(rows, axis=1)
+                columns.append(column)
+            
+            combined_large_tiles = np.concatenate(columns, axis=2)
+            print('All large tiles shape', combined_large_tiles.shape)
 
-        # Calculate band averages across this sub-tile
-        band_averages = np.mean(subtile, axis=(1, 2))
-        print("Band averages", band_averages)
-        fraction_missing_reflectance = band_averages[-1]
+            # Find indices of bounding box within this combined large tile 
+            top_idx, left_idx = lat_long_to_index(max_lat, min_lon, max_lat_tile_top, min_lon_tile_left, RES)
+            bottom_idx = top_idx + SUBTILE_PIXELS
+            right_idx = left_idx + SUBTILE_PIXELS
+            print('From combined large tile: Top', top_idx, 'Botom', bottom_idx, 'Left', left_idx, 'Right', right_idx)
 
-        # If there's too much missing reflectance data, skip this point
-        if fraction_missing_reflectance > MAX_FRACTION_MISSING_OVERALL:
-            missing_points += 1
-            print('Too much missing')
-            continue
+            # If the selected region (box) goes outside the range of the cover or reflectance dataset, that's a bug!
+            if top_idx < 0 or left_idx < 0:
+                print("Index was negative!")
+                exit(1)
+            if (bottom_idx >= combined_large_tiles.shape[1] or right_idx >= combined_large_tiles.shape[2]):
+                print("Reflectance index went beyond edge of array!")
+                exit(1)
 
-        # If too much CDL data is missing, skip this point
-        cdl_coverage = np.sum(band_averages[CDL_BANDS])
-        if cdl_coverage < MIN_CDL_COVERAGE:
-            print('CDL coverage too low:', cdl_coverage)
-            continue
+            subtile = combined_large_tiles[:, top_idx:bottom_idx, left_idx:right_idx]
 
-        if band_averages[13] > PURE_THRESHOLD:
-            pure_corn_points += 1
-            print('Pure corn')
-        if band_averages[14] > PURE_THRESHOLD:
-            pure_soy_points += 1
-            print('Pure soy')
+            # Calculate band averages across this sub-tile
+            band_averages = np.mean(subtile, axis=(1, 2))
+            print("Band averages", band_averages)
+            fraction_missing_reflectance = band_averages[-1]
+
+            # If there's too much missing reflectance data, skip this point
+            # if fraction_missing_reflectance > MAX_FRACTION_MISSING_OVERALL:
+            #     missing_points += 1
+            #     print('Too much missing')
+            #     continue
+
+            # If too much CDL data is missing, skip this point
+            cdl_coverage = np.sum(band_averages[CDL_BANDS])
+            if cdl_coverage < MIN_CDL_COVERAGE:
+                print('CDL coverage too low:', cdl_coverage)
+                continue
+
+            if band_averages[13] > PURE_THRESHOLD:
+                pure_corn_points += 1
+                print('Pure corn')
+            if band_averages[14] > PURE_THRESHOLD:
+                pure_soy_points += 1
+                print('Pure soy')
 
 
-        # Save the sub-tiles array to file
-        subtile_filename = OCO2_SUBTILES_DIR + "/lat_" + str(lat) + "_lon_" + str(lon) + ".npy"
-        np.save(subtile_filename, subtile)
+            # Save the sub-tiles array to file
+            subtile_filename = OCO2_SUBTILES_DIR + "/lat_" + str(lat) + "_lon_" + str(lon) + ".npy"
+            np.save(subtile_filename, subtile)
 
-        point_lons.append(lon)
-        point_lats.append(lat)
-        point_sifs.append(sif)
+            point_lons.append(lon)
+            point_lats.append(lat)
+            point_sifs.append(sif)
 
-        dataset_rows.append([lon, lat, DATE, subtile_filename] + band_averages.tolist() + 
-                             [sif, num_soundings_point])
-        
+            dataset_rows.append([lon, lat, DATE, subtile_filename] + band_averages.tolist() + 
+                                [sif, num_soundings_point])
+            
 
-# Write dataset to file
-with open(OUTPUT_CSV_FILE, "w") as output_csv_file:
-    csv_writer = csv.writer(output_csv_file, delimiter=",", quoting=csv.QUOTE_MINIMAL)
-    for row in dataset_rows:
-        csv_writer.writerow(row) 
+    # Write dataset to file
+    with open(os.path.join(DATASET_DIR, OUTPUT_CSV_FILENAME), "w") as output_csv_file:
+        csv_writer = csv.writer(output_csv_file, delimiter=",", quoting=csv.QUOTE_MINIMAL)
+        for row in dataset_rows:
+            csv_writer.writerow(row) 
+
+
 
 print('Missing points', missing_points)
 print('Pure corn points', pure_corn_points)