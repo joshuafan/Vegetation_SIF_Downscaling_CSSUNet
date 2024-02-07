9b42cfff803c4463760801ca2b390d4f60bbcf2d
Joshua Fan <jyf6@cornell.edu>
2024-02-07 10:38:00 -0500

    Remove old files

--- old_files/OLD_run_train_VI.sh
+++
@@ -1,3 +0,0 @@
-# OLD - not working
-python3 train_downscaling_unet.py --prefix 10d --model unet2 --optimizer Adam -lr 1e-4 -wd 1e-4 -sche const -epoch 50 -bs 64 \
-    --fraction_outputs_to_average 1 --flip_and_rotate --jigsaw  --seed 0 #--compute_vi
